f8286377e0854b141ca420c9853fbba8d212e294
Derek YIP <derekyip@esdlife.com>
2022-06-30 10:19:42 +0800

    git

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -21,9 +21,10 @@ function MovieDetail() {
         <img src={"https://image.tmdb.org/t/p/w400/" + Detail.poster_path} alt="" />
         <div className="detail_right">
           <h1>{Detail.title}</h1>
-          <p>{Detail.vote_average}</p>
-          <p>{Detail.release_date}</p>
-          <p>{Detail.overview}</p>
+          <p>Average Score: {Detail.vote_average} /10</p>
+          <p>Release Date: {Detail.release_date}</p>
+          <p>Movie Overview: </p>
+          <p>{Detail.overview} Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus perferendis, modi magnam molestias perspiciatis placeat atque dolore, possimus, odio provident nesciunt. Numquam, corporis dolore tempora, magni recusandae illo minima perferendis aspernatur veniam exercitationem ratione rerum voluptatibus itaque illum possimus. Error eveniet repudiandae, amet incidunt beatae mollitia. Praesentium a explicabo consequatur tempora ipsa perspiciatis consequuntur id. Qui laborum dolorum quam mollitia illo neque molestias quod culpa. Fugiat dignissimos corporis nesciunt eos placeat beatae tempore quia minima quis modi? Enim nulla iusto perferendis omnis ex ipsa doloribus, soluta est nisi velit, hic beatae non nobis, at eos laborum debitis atque. Ut, optio.</p>
         </div>
     </div>
   )
